e61211e78bbf93efb063f8afd64ef1854ec23cea
张大嘴 <zyb16@msn.cn>
2018-12-11 15:06:10 +0800

    添加项目文件。

---
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows;
+
+namespace CefSharp.MinimalExample.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            request request = new request();
+            request.NotifyMsg += Request_NotifyMsg;
+            Browser.RequestHandler = request;
+            Browser.LifeSpanHandler = new OpenPageSelf();
+        }
+        int index = 0;
+        private void Request_NotifyMsg(byte[] obj)
+        {
+            using (FileStream fs = new FileStream(Browser.Title+ ++index + ".ts", FileMode.Create))
+            {
+                fs.Position = fs.Length;
+                fs.Write(obj, 0, obj.Length);
+                fs.Close();
+            }
+        }
+    }
+   
+}
---
+++ 视频下载器/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 视频下载器
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            request request = new request(new List<string>() { ".ts" });
+            request.NotifyMsg += Request_NotifyMsg;
+            Browser.RequestHandler = request;
+            Browser.LifeSpanHandler = new OpenPageSelf();
+            Browser.TitleChanged += Browser_TitleChanged;
+        }
+        string bwTitle = "NOOP";
+        long index = 0;
+        private void Browser_TitleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var bw = sender as CefSharp.Wpf.ChromiumWebBrowser;
+            bwTitle = bw.Title;
+            index = 0;
+            //throw new NotImplementedException();
+        }
+
+        
+        private void Request_NotifyMsg(byte[] obj)
+        {
+            using (FileStream fs = new FileStream(bwTitle + ++index + ".ts", FileMode.Create))
+            {
+                fs.Position = fs.Length;
+                fs.Write(obj, 0, obj.Length);
+                fs.Close();
+            }
+        }
+
+        private void BtnGO_Click(object sender, RoutedEventArgs e)
+        {
+            //if(IsUrl(tbUrl.Text))
+            //{
+            //    Browser.Address = tbUrl.Text;
+            //}
+            Browser.Address = tbUrl.Text;
+            
+        }
+        public static bool IsUrl(string str)
+        {
+            try
+            {
+                string Url = @"^http(s)?://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)?$";
+                return Regex.IsMatch(str, Url);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
